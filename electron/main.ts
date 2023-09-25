71c65258099e5a62d75b689dbb31021b14b21a89
mikecebul <mikecebul@gmail.com>
2023-09-25 02:36:44 -0400

    Add Start meeting functions

--- electron/main.ts
+++ electron/main.ts
@@ -1,5 +1,8 @@
-import { app, BrowserWindow } from 'electron'
-import path from 'node:path'
+import { app, BrowserWindow, ipcMain } from "electron";
+import path from "node:path";
+import open from "open";
+import axios from "axios";
+import fs from "fs";
 
 // The built directory structure
 //
@@ -10,51 +13,128 @@ import path from 'node:path'
 // │ │ ├── main.js
 // │ │ └── preload.js
 // │
-process.env.DIST = path.join(__dirname, '../dist')
-process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
+process.env.DIST = path.join(__dirname, "../dist");
+process.env.VITE_PUBLIC = app.isPackaged
+  ? process.env.DIST
+  : path.join(process.env.DIST, "../public");
 
-
-let win: BrowserWindow | null
+let win: BrowserWindow | null;
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
-  })
+  });
 
   // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
+  win.webContents.on("did-finish-load", () => {
+    win?.webContents.send("main-process-message", new Date().toLocaleString());
+  });
 
   if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
+    win.loadURL(VITE_DEV_SERVER_URL);
   } else {
     // win.loadFile('dist/index.html')
-    win.loadFile(path.join(process.env.DIST, 'index.html'))
+    win.loadFile(path.join(process.env.DIST, "index.html"));
   }
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-    win = null
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+    win = null;
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-app.whenReady().then(createWindow)
+interface IZoomToken {
+  access_token: string;
+}
+
+ipcMain.on("start-zoom-meeting", async () => {
+  const token = await getZoomToken();
+  if (token) {
+    await startZoomMeeting(token);
+  }
+});
+
+async function getZoomToken<T extends IZoomToken>(): Promise<T | undefined> {
+  const accountId = import.meta.env.VITE_S2S_ACCOUNT_ID;
+  const clientId = import.meta.env.VITE_S2S_CLIENT_ID;
+  const clientSecret = import.meta.env.VITE_S2S_CLIENT_SECRET;
+
+  const base64Credentials = Buffer.from(`${clientId}:${clientSecret}`).toString(
+    "base64"
+  );
+  const url = `https://zoom.us/oauth/token?grant_type=account_credentials&account_id=${accountId}`;
+
+  try {
+    const response = await axios.post(
+      url,
+      {},
+      {
+        headers: {
+          Host: "zoom.us",
+          Authorization: `Basic ${base64Credentials}`,
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.log(`Failed to fetch, status: ${response.status}`);
+      return undefined;
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    fs.writeFileSync(
+      "/home/mike/Documents/error-logs/start-meeting-error-log.txt",
+      `Axios Error during getZoomToken: ${error}\n`
+    );
+    return undefined;
+  }
+}
+
+async function startZoomMeeting<T extends IZoomToken>(token: T) {
+  const meetingId = import.meta.env.VITE_MEETING_ID;
+  const bearerToken = token.access_token;
+
+  const url = `https://api.zoom.us/v2/meetings/${meetingId}`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${bearerToken}`,
+      },
+    });
+    if (response.status === 200) {
+      const startUrl = response.data["start_url"];
+      console.log("Start Url:", startUrl);
+      open(startUrl);
+    }
+  } catch (error) {
+    console.error("Error starting the meeting:", error);
+    fs.writeFileSync(
+      "/home/mike/Documents/error-logs/start-meeting-error-log.txt",
+      `Caught exception in startZoomMeeting: ${error}\n`
+    );
+  }
+}
+
+app.whenReady().then(createWindow);
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./main.css";
 
 function App() {
+  const startMeeting = () => {
+    window.electronAPI?.startZoomMeeting();
+  };
   return (
     <main className="flex flex-col justify-center items-center h-[100dvh] bg-slate-900 text-slate-100">
       <p>Welcome to the</p>
       <h1 className="text-5xl">Serenity House</h1>
       <div className="pt-32">
         <button
-          id="startZoom"
+          onClick={startMeeting}
           className="px-6 py-2 text-xl font-semibold tracking-wide text-center text-black transition duration-200 ease-in-out bg-orange-600 rounded-lg shadow-lg hover:shadow-xl hover:bg-orange-500 focus:outline-none focus:border-orange-700 focus:ring focus:ring-orange-200 focus:ring-opacity-50"
         >
           Start Hybrid Meeting
